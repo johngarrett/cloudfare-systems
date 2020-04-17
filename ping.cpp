9053227ad1a1f0b6b23fab9160d39064ff6fb6c7
John Garrett <jacgarrett18@gmail.com>
2020-04-16 20:27:16 -0400

    ipv6 support... somehow

--- ping.cpp
+++ ping.cpp
@@ -5,19 +5,24 @@
  * create an ifdef linux icmp.h is avalible
  */
 // #include <netinet/ip_icmp.h> 
+#include <netinet/icmp6.h>
+#include <netinet/ip6.h>
+
+
 #include <iostream>
 #include <netinet/in.h> // sockaddr_in
 #include <cstdio>
 #include <cstdlib>
 #include <stdint.h>
-#include <linux/icmp.h>
-#include <linux/ip.h>
+//#include <linux/icmp.h>
+//#include <linux/icmpv6.h>
+//#include <linux/ip.h>
+//#include <linux/ipv6.h> shouldn't be using these allegedly
 
-#include <time.h>
-#include <errno.h>
 #include <cstring> // string arrays
-#include <netdb.h>
 #include <unistd.h> // for closing sockets
+#include <errno.h>
+
 
  int32_t ping::checksum(uint16_t *buf, int32_t len) {
     int32_t nleft = len;
@@ -44,7 +49,7 @@
     return answer;
 }
 
-void ping::start_ping(std::string _host) {
+void ping::start_ping(std::string dest) {
              
     /*
      * domain = 
@@ -56,7 +61,7 @@ void ping::start_ping(std::string _host) {
      *   for ipv4, this number is 0
      */
 
-    int sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    int sock = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
 
     if (sock < 0) { // an error occured
         close(sock);
@@ -73,25 +78,20 @@ void ping::start_ping(std::string _host) {
     /*
      * sockaddr_in fields: {sin_family, sin_port, sin_addr, sin_zero}
      */
-    sockaddr_in pingaddr; 
-    memset(&pingaddr, 0, sizeof(sockaddr_in)); // TODO: see if you can remove this (i think its for sin_zero)
-    pingaddr.sin_family = AF_INET;
+    sockaddr_in6 pingaddr; 
+    pingaddr.sin6_family = AF_INET6;
      
     /*
      *TODO: remove this, i think its just getting the current machine's ip address in a really complicated way
      * or, its a way to verify the destination (i think this is more correct)
      */
-    std::cout << _host << "\n";
+    std::cout << dest << "\n";
 
-    hostent *h = gethostbyname(_host.c_str());
-    if(not h)
-    {
-        printf("Failed to get host by name!\n");
-        close(sock);
-        exit(1);
-    }
-    
-    memcpy(&pingaddr.sin_addr, h->h_addr, sizeof(pingaddr.sin_addr)); // copy host address into .sin_addr
+    struct sockaddr_in6 host;
+    inet_pton(AF_INET6, dest.c_str(), &host.sin6_addr); // convert ip address from text to binary
+
+    pingaddr.sin6_addr = host.sin6_addr;
+    std::cout << pingaddr.sin6_addr.s6_addr <<"\n";
 
     /*
      * strange error:
@@ -109,21 +109,25 @@ void ping::start_ping(std::string _host) {
      * when listening for an echo, this is everything we want to filter out
      * TODO: make this not bitwise bullshit
      */
-    icmp_filter filter;
-    filter.data = ~((1<<ICMP_SOURCE_QUENCH) |
-                    (1<<ICMP_DEST_UNREACH) |
-                    (1<<ICMP_TIME_EXCEEDED) |
-                    (1<<ICMP_REDIRECT) |
-                    (1<<ICMP_ECHOREPLY));
+    icmp6_filter filter;
+    //(1<<ICMP6_SOURCE_QUENCH)  is now depricated
+    /* 
+    filter.data = ~((1<<ICMP6_DEST_UNREACH) |
+                    (1<<ICMP6_TIME_EXCEED) |
+                    (1<<ICMP6_ECHO_REPLY));
+                */
     /*
      * >... manipulate options for the socket referred to by the file descriptor sockfd
      * >... 
        Raw socket options can be set  with  getsockopt  and  read
        with getsockopt by passing the SOL_RAW family flag.
+
+TODO: change IPPROTTO back to SOL_RAW?
      */
-    if(setsockopt(sock, SOL_RAW, ICMP_FILTER, (char *)&filter, sizeof(filter)) < 0) {
-        std::cout << "couldn't set socket options... dont know why";
-        throw std::runtime_error(" ");
+    if(setsockopt(sock, IPPROTO_ICMPV6, ICMP6_FILTER, (char *)&filter, sizeof(filter)) < 0) {
+                printf ("Error %d opening socket.\n", errno);
+
+        throw std::runtime_error("couldn't set socket options... dont know why");
     }
 
     unsigned short num_recieved = 0;
@@ -137,36 +141,36 @@ void ping::start_ping(std::string _host) {
     std::cout << "we recived " << num_recieved << " packets back";
 }
 
-void ping::send_imcp_echo_packet(int sock, sockaddr_in pingaddr, unsigned short seq, unsigned short id) {
-    char packet[sizeof(icmphdr)];
+void ping::send_imcp_echo_packet(int sock, sockaddr_in6 pingaddr, unsigned short seq, unsigned short id) {
+    char packet[sizeof(icmp6_hdr)];
     memset(packet, 0, sizeof(packet)); // fill with zeros to ensure no corruption or anything
     
-    icmphdr *pkt = (icmphdr *)packet; // everything inside icmphdr is an int or struct of ints
-    pkt->type = ICMP_ECHO; // ICMP ECHO for out, ICMP_ECHO_REPLY will come back to us
-    pkt->code = 0; // code 0 = echo reply/req
-    pkt->un.echo.id = htons(id & 0xFFFF); // htons converts the unsigned integer hostlong to network bye order
-    pkt->un.echo.sequence = seq; 
-    pkt->checksum = checksum((uint16_t *) pkt, sizeof(packet));
+    icmp6_hdr *pkt = (icmp6_hdr *)packet; // everything inside icmp6_hdr is an int or struct of ints
+    pkt->icmp6_type = ICMP6_ECHO_REQUEST; // ICMP6 ECHO for out, ICMP6_ECHO_REPLY will come back to us
+    pkt->icmp6_code = 0; // code 0 = echo reply/req
+    pkt->icmp6_id = htons(id & 0xFFFF); // htons converts the unsigned integer hostlong to network bye order
+    pkt->icmp6_seq = seq; 
+    pkt->icmp6_cksum = 0;//checksum((uint16_t *) pkt, sizeof(packet));
     //pkt->checksum = 0;
 
-    int bytes = sendto(sock, packet, sizeof(packet), 0 /*flags*/, (sockaddr *)&pingaddr, sizeof(sockaddr_in));
+    int bytes = sendto(sock, packet, sizeof(packet), 0 /*flags*/, (sockaddr *)&pingaddr, sizeof(sockaddr_in6));
 
     if (bytes < 0) {
         std::cout << "the sendto command returned " << bytes << "... we cant send to reciver";
         close(sock);
-        throw std::runtime_error(" ");
+        throw std::runtime_error("bytes were less than 0");
     }
 
     else if (bytes != sizeof(packet)) {
         std::cout << "We couldn't write the whole package..\n";
         std::cout << bytes <<"\t versus expect size of: " << sizeof(packet);
         close(sock);
-        throw std::runtime_error(" ");
+        throw std::runtime_error("we couldn't write the whole packet ");
     }
 }
 
 
-unsigned short ping::listen_for_reply(int sock, sockaddr_in pingaddr, unsigned short id) {
+unsigned short ping::listen_for_reply(int sock, sockaddr_in6 pingaddr, unsigned short id) {
     while (1) {
             char inbuf[192]; // TODO: find out what this 192 is
             memset(inbuf, 0, sizeof(inbuf));
@@ -179,19 +183,17 @@ unsigned short ping::listen_for_reply(int sock, sockaddr_in pingaddr, unsigned s
                 std::cout << "[LISTEN] bytes found: " << bytes << "\n\twe expect a value > 0... continuing\n";
                 continue;
             }
-
             else {
-                if (bytes < sizeof(iphdr) + sizeof(icmphdr)) { // we're getting back an icmphdr wrapped in an ip header TODO cehck
+                if (bytes < sizeof(icmp6_hdr)) { // we're getting back an icmp6_hdr wrapped in an ip header TODO cehck
                     std::cout << "[LISTEN] Incorrect read bytes!\n\t... continuing\n";
                 }
 
-                iphdr *iph = (iphdr *)inbuf;
-                int hlen = (iph->ihl << 2); // shift left 2
-                bytes -= hlen; // subtract the ip header from the bytes, we only care about the icmp info
+                ip6_hdr *iph = (ip6_hdr *)inbuf;
+                // headerlength is automtically cropped out?????????? 
                 
-                icmphdr *pkt = (icmphdr *)(inbuf + hlen); // at this point, inbuf + hlen points to the icmp header
-                int extracted_id = ntohs(pkt->un.echo.id); // converts unsigned int from network to host byte order (opposite of the htons we did earlier! ish)
-                if (pkt->type == ICMP_ECHOREPLY) {
+                icmp6_hdr *pkt = (icmp6_hdr *)(inbuf ); // at this point, inbuf + hlen points to the icmp header
+                int extracted_id = ntohs(pkt->icmp6_id); // converts unsigned int from network to host byte order (opposite of the htons we did earlier! ish)
+                if (pkt->icmp6_type == ICMP6_ECHO_REPLY) {
                     std::cout << "WE found an IMCP ECHO REPLY!\n";
                     if (extracted_id == id) {
                         std::cout << "The pid's matched!\n";
@@ -202,14 +204,14 @@ unsigned short ping::listen_for_reply(int sock, sockaddr_in pingaddr, unsigned s
                         return 0;
                     }
                 }
-                else if (pkt->type == ICMP_DEST_UNREACH) {
-                    std::cout << "[LISTEN] packet type of ICMP_DEST_UNREACH\n";
+                else if (pkt->icmp6_type == ICMP6_DST_UNREACH) {
+                    std::cout << "[LISTEN] packet type of ICMP6_DEST_UNREACH\n";
 
-                    int offset = sizeof(iphdr) + sizeof(icmphdr) + sizeof(iphdr); // iphdr + icmp hdr going out + another iphdr coming back in? TODO
-                    if (((bytes + hlen) - offset) == sizeof(icmphdr))
+                    int offset = sizeof(ip6_hdr) + sizeof(icmp6_hdr) + sizeof(ip6_hdr); // ip6_hdr + icmp hdr going out + another ip6_hdr coming back in? TODO
+                    if (((bytes ) - offset) == sizeof(icmp6_hdr))
                     {
-                        icmphdr *p = reinterpret_cast<icmphdr *>(inbuf + offset); // extract the original icmp packet
-                        if (ntohs(p->un.echo.id) == id) {
+                        icmp6_hdr *p = reinterpret_cast<icmp6_hdr *>(inbuf + offset); // extract the original icmp packet
+                        if (ntohs(p->icmp6_id) == id) {
                             std::cout << "\tID's match, destination is unreachable\n";
                             return 0;
                         }
--- ping.h
+++ ping.h
@@ -7,7 +7,7 @@ class ping
         static void start_ping(std::string _host);
     private:
         static int32_t checksum(uint16_t *buf, int32_t len);
-        static void send_imcp_echo_packet(int, sockaddr_in, unsigned short, unsigned short);
+        static void send_imcp_echo_packet(int, sockaddr_in6, unsigned short, unsigned short);
         /**
          * listen for reply on one packet
          *
@@ -17,5 +17,5 @@ class ping
          * @param pingaddr the address to recieve packets from
          * @param id the id attached to all outgoing packets
          */
-        static unsigned short listen_for_reply(int sock, sockaddr_in pingaddr, unsigned short id);
+        static unsigned short listen_for_reply(int sock, sockaddr_in6 pingaddr, unsigned short id);
 };
